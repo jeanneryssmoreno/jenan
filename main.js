a7557fca8263a6a5a27243294f91cb800955169a
jeanneryssmoreno <morenojeannerys@gmail.com>
2024-06-02 13:04:28 -0400

    Readme

---
+++ main.js
@@ -0,0 +1,9 @@
+// import './style.css'
+
+document.querySelector('#app').innerHTML = `
+ <h1>hello people <h1/>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+     
+`;
+
+
